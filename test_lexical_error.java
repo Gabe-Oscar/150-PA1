80886b5947c557bdf96e6d5fc16656860dda3d77
Billy-FIN <billyfinnn@gmail.com>
2024-02-04 19:54:51 -0500

    finished minijavaLex.jj and included a test program for the lexical error

---
+++ test_lexical_error.java
@@ -0,0 +1,7 @@
+class Factorial {
+    public static void main(String[] a) {
+        //_d is not a valid identifier, "_" will be recognized as OTHER
+        _d = 1;
+        System.out.println(new Fac().ComputeFac(10));
+    }
+}
